b2773b3ec2091b4536d1f8c68ab6936149137f72
haved <krogstie.havard@gmail.com>
2016-06-23 23:53:58 +0200

    Add BaseEngine and TestGame implementation

---
+++ src/me/hyv/of/engine/BaseEngine.java
@@ -0,0 +1,102 @@
+package me.hyv.of.engine;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.glfw.GLFW.*;
+
+public class BaseEngine {
+	
+	private AbstractGame game;
+	private String title;
+	private int w_width, w_height;
+	private boolean vsync;
+	@SuppressWarnings("unused")
+	private boolean fullscreen;
+	
+	private long windowHandle;
+	
+	public BaseEngine(AbstractGame game, String title, int width, int height, boolean vsync, boolean fullscreen) {
+		this.game = game;
+		this.title = title;
+		this.w_width = width;
+		this.w_height = height;
+		this.vsync = vsync;
+		this.fullscreen = fullscreen;
+	}
+	
+	public void run() {
+		try {
+			GLFWErrorCallback.createPrint(System.err).set();
+			makeWindowAndContext();
+			centerWindow();
+			makeKeyCallback();
+			glfwMakeContextCurrent(windowHandle);
+	        glfwSwapInterval(vsync?1:0);
+	        GL.createCapabilities();
+	        
+	        game.init(this);
+	        makeResizeCallback();
+	        glfwShowWindow(windowHandle);
+	        loop();
+		} finally {
+			game.destroy();
+			glfwSetKeyCallback(windowHandle, null).free();
+			glfwSetFramebufferSizeCallback(windowHandle, null).free();
+			glfwTerminate();
+			glfwSetErrorCallback(null).free();
+		}
+	}
+	
+	private void makeWindowAndContext() {
+		if(!glfwInit())
+			  throw new IllegalStateException("Unable to initialize GLFW");
+		
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		windowHandle = glfwCreateWindow(w_width, w_height, title, NULL, NULL);
+		if(windowHandle == NULL)
+			throw new RuntimeException();
+	}
+	
+	private void makeKeyCallback() {
+		GLFWKeyCallback.create((window, key, scancode, action, mods)->{
+			game.onKeyStateChange(key, action);
+		}).set(windowHandle);
+	}
+	
+	private void centerWindow() {
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		
+		int[] w = new int[1], h = new int[1];
+		glfwGetWindowSize(windowHandle, w, h);
+		
+        glfwSetWindowPos(
+            windowHandle,
+            (vidmode.width() - w[0]) / 2,
+            (vidmode.height() - h[0]) / 2
+        );
+	}
+
+	private void makeResizeCallback() {
+		GLFWFramebufferSizeCallback.create((window, width, height)->game.resize(width, height)).set(windowHandle);
+	}
+	
+	private void loop() {
+		long prevTime = System.currentTimeMillis()-16; //16 milliseconds is a 
+		while(!glfwWindowShouldClose(windowHandle)) {
+			game.onFrame((int)-(prevTime-(prevTime=System.currentTimeMillis())));
+			glfwSwapBuffers(windowHandle);
+			glfwPollEvents();
+		}
+	}
+	
+	public void closeWindow() {
+		glfwSetWindowShouldClose(windowHandle, true);
+	}
+}
